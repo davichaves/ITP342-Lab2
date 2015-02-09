9c83ec7b13a749448855edd8e6db1966c891caac
Davi Chaves <dchaves@usc.edu>
2015-02-09 12:52:18 -0800

    Initial Commit

---
+++ Story/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Story
+//
+//  Created by Davi Chaves on 2/9/15.
+//  Copyright (c) 2015 Davi Chaves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Story/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Story
+//
+//  Created by Davi Chaves on 2/9/15.
+//  Copyright (c) 2015 Davi Chaves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

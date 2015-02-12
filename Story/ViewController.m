a6bda9eb649a45c21567210e04f406f8d628a2f2
Davi Chaves <dchaves@usc.edu>
2015-02-11 23:52:46 -0800

    submission

--- Story/ViewController.m
+++ Story/ViewController.m
@@ -10,28 +10,68 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *More;
+@property (weak, nonatomic) IBOutlet UITextField *Name;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (nonatomic, strong) IBOutlet UISlider *slider;
 
 @end
 
 @implementation ViewController {
+    NSString *name;
     bool simple;
 }
 
 - (void)viewDidLoad {
+    simple = true;
+    [_More setHidden:true];
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 - (IBAction)simpleOfMore:(id)sender {
-    if (UISegmentedControlSegmentLeft) {
+    UISegmentedControl *segmentedControl = (UISegmentedControl *) sender;
+    NSInteger selectedSegment = segmentedControl.selectedSegmentIndex;
+    
+    if (selectedSegment == 0) {
+        //toggle the correct view to be visible
         simple = true;
         [_More setHidden:true];
-        [_More setAlpha:0];
-    }
-    if (UISegmentedControlSegmentRight) {
+    } else{
+        //toggle the correct view to be visible
         simple = false;
         [_More setHidden:false];
-        [_More setAlpha:1];
     }
+}
+- (IBAction)stepper:(id)sender {
+    
+}
+- (IBAction)slider:(id)sender {
+    if (sender == _slider) {
+        _sliderLabel.text = [NSString stringWithFormat:@"%f", _slider.value];
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)Name {
+    
+}
+- (IBAction)createStory:(id)sender {
+    UIActionSheet *popup = [[UIActionSheet alloc] initWithTitle:@"Select Sharing option:" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:
+                            @"Share on Facebook",
+                            nil];
+    popup.tag = 1;
+    [popup showInView:[UIApplication sharedApplication].keyWindow];
+}
+
+-(BOOL) textFieldShouldReturn: (UITextField *)Name {
+    name = _Name.text;
+    [Name resignFirstResponder];
+    [self.view endEditing:YES];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
